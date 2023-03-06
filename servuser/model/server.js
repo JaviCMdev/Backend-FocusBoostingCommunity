ff69fc1c5d3d28cb8676934ffd35f03507b7d62c
JaviCMDev <capillaj.dev@gmail.com>
2023-03-06 20:52:58 +0100

    Creacion MVC User Server

---
+++ servuser/model/server.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const serverSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    location: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+const Server = mongoose.model('Server', serverSchema);
+module.exports = Server;
